09ac8cb063244a3c8a80f57273389df52c0f25ff
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-11-28 21:45:47 +0200

    adding sorting functionality

--- app/data/projects/project-table.tsx
+++ app/data/projects/project-table.tsx
@@ -1,18 +1,24 @@
 'use client'
 
 import { DataTable } from "@/components/data-table";
-import { columns } from "@/components/project/columns";
+import { columns, myTasksColumns } from "@/components/project/columns";
 import { File, Project, Task, User } from "@prisma/client";
 
-interface TaskProps extends Task {
+export interface TaskProps extends Task {
 	assignedTo: User;
 	project: Project;
 	attachments: File[];
 }
 
-export const ProjectTable = ({tasks}: {tasks:TaskProps[]}) => {
+export const ProjectTable = ({ tasks }: { tasks: TaskProps[] }) => {
 	return (
 		<DataTable columns={columns} data={tasks as any} />
 
+	)
+}
+
+export const MyTasksTable = ({ tasks }: { tasks: TaskProps[] }) => {
+	return (
+		<DataTable columns={myTasksColumns} data={tasks as any} />
 	)
 }
